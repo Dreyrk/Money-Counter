f2d67db06f7e6a3b93f054b43fb2ab4d9219013e
Dreyrk <lucas.rondepierre123@gmail.com>
2023-10-19 12:52:43 +0200

    start form for add spending

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,7 +1,30 @@
 export default function Page() {
+  const createSpending = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const title = formData.get("title");
+    const amount = formData.get("amount");
+    const description = formData.get("description");
+  };
+
   return (
     <div>
-      <h1>Add a spend page</h1>
+      <form action={createSpending}>
+        <div>
+          <label htmlFor="title">Titre</label>
+          <input type="text" id="title" name="title" />
+        </div>
+        <div>
+          <label htmlFor="amount">Montant</label>
+          <input type="number" id="amount" name="amount" />
+        </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <textarea id="description" name="description" />
+        </div>
+        <button type="submit">Ajouter une dépense</button>
+      </form>
     </div>
   );
 }
