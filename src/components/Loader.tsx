5628d0357dcd9331e4b953517c737ab4bbecfca7
Dreyrk <lucas.rondepierre123@gmail.com>
2023-10-24 19:54:10 +0200

    fix loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-export default function Loader({ size = 16 }: { size?: number }) {
+export default function Loader({ size = 64 }: { size?: number }) {
   return (
     <div
-      className={`w-${size || 16} h-${
-        size || 16
-      } border-8 border-t-8 rounded-full place-self-center border-light-black border-t-blue-600 animate-spin`}
+      style={{ height: `${size}px`, width: `${size}px` }}
+      className="border-8 rounded-full place-self-center border-light-black border-t-purple-600 animate-spin"
     />
   );
 }
